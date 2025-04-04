2a1f872e848933f6c2a26d803883e8c00fbd5ea5
Thayse-Dias <156327575+Thayse-Dias@users.noreply.github.com>
2025-04-04 12:18:52 -0300

    Update port.js

--- services/port.js
+++ services/port.js
@@ -1,1 +1,21 @@
+const net = require('net');
 
+const findAvailablePort = (startPort) => {
+  return new Promise((resolve) => {
+    const server = net.createServer();
+    server.listen(startPort, () => {
+      const { port } = server.address();
+      server.close(() => resolve(port));
+    });
+    server.on('error', (err) => {
+      if (err.code === 'EADDRINUSE') {
+        server.close(() => findAvailablePort(startPort + 1).then(resolve));
+      } else {
+        console.error('Erro ao buscar porta:', err.message);
+        process.exit(1);
+      }
+    });
+  });
+};
+
+module.exports = { findAvailablePort };
