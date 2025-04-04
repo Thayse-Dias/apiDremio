b897c0258bc5451a607e888efcc2313e8950bdf2
Thayse-Dias <156327575+Thayse-Dias@users.noreply.github.com>
2025-04-04 12:16:58 -0300

    Update testConnection.js

--- routes/testConnection.js
+++ routes/testConnection.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const router = express.Router();
 
+router.get('/test-connection', (req, res) => {
+  res.json({
+    internalUrl: 'http://dremio-adapter:3005',
+    externalUrl: `http://localhost:3002`,
+    status: 'active',
+  });
+});
+
+module.exports = router;
